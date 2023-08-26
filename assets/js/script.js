6297db25da6e4287773dae8800c9a96314c64d0c
todiane <>
2023-08-26 12:55:45 +0000

    Update js code to make sync work. still issue with not going to correct part of video

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,6 @@
 const transcript = document.getElementById('transcript');
 const video = document.getElementById('video');
 
-transcript.addEventListener('click', (event) => {
-    if (event.target.tagName === 'P') {
-        const time = parseFloat(event.target.getAttribute('data-time'));
-        video.currentTime = time;
-    }
-});
-
-// Bold current paragraph
-video.addEventListener('timeupdate', () => {
-    const currentTime = video.currentTime;
-
-    const currentParagraph = transcript.querySelector('p.current');
-    if (currentParagraph) {
-        currentParagraph.classList.remove('current');
-    }
-
-    const newParagraph = transcript.querySelector(`p[data-time="${currentTime}"]`);
-    if (newParagraph) {
-        newParagraph.classList.add('current');
-    }
-});
-
-// Scroll transcript to current line
 video.addEventListener('timeupdate', () => {
     const currentTime = video.currentTime;
 
@@ -38,3 +15,15 @@ video.addEventListener('timeupdate', () => {
         newParagraph.scrollIntoView();
     }
 });
+
+transcript.addEventListener('click', (event) => {
+    if (event.target.tagName === 'P') {
+        const time = parseFloat(event.target.getAttribute('data-time'));
+        video.currentTime = time;
+        event.target.scrollIntoView(); // Scroll the clicked paragraph into view
+    }
+});
+
+
+
+
