7a7ee0efff292da64a6b5e7b5b7e1bb8e376b1b8
todiane <>
2023-08-26 20:35:05 +0000

    removed claude.ai js and returned to older version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,30 @@
 
 video.addEventListener('loadedmetadata', () => {
-    // Get elements
-    const video = document.getElementById('video');
+ 
     const transcript = document.getElementById('transcript');
+    const video = document.getElementById('video');
 
-    // Highlight transcript on timeupdate 
-    video.addEventListener('timeupdate', highlightTranscript);
-
-    function highlightTranscript() {
-
+    video.addEventListener('timeupdate', () => {
         const currentTime = video.currentTime;
 
-        // Remove any existing highlight
-        transcript.querySelector('.highlighted')?.classList.remove('highlighted');
-
-        // Highlight current paragraph
-        transcript.querySelector(`p[data-time="${currentTime}"]`).classList.add('highlighted');
-
-    }
-
-    // Handle clicks on transcript
-    transcript.addEventListener('click', goToParagraph);
-
-    function goToParagraph(e) {
-        if (e.target.tagName === 'P') {
-            const time = e.target.getAttribute('data-time');
-            video.currentTime = time;
+        const currentParagraph = transcript.querySelector('p.current');
+        if (currentParagraph) {
+            currentParagraph.classList.remove('current');
         }
-    }
 
+        const newParagraph = transcript.querySelector(`p[data-time="${currentTime}"]`);
+        if (newParagraph) {
+            newParagraph.classList.add('current');
+            newParagraph.scrollIntoView();
+        }
+    });
 
-    // const transcript = document.getElementById('transcript');
-    // const video = document.getElementById('video');
-
-    // video.addEventListener('timeupdate', () => {
-    //     const currentTime = video.currentTime;
-
-    //     const currentParagraph = transcript.querySelector('p.current');
-    //     if (currentParagraph) {
-    //         currentParagraph.classList.remove('current');
-    //     }
-
-    //     const newParagraph = transcript.querySelector(`p[data-time="${currentTime}"]`);
-    //     if (newParagraph) {
-    //         newParagraph.classList.add('current');
-    //         newParagraph.scrollIntoView();
-    //     }
-    // });
-
-    // transcript.addEventListener('click', (event) => {
-    //     if (event.target.tagName === 'p') {
-    //         const time = parseFloat(event.target.getAttribute('data-time'));
-    //         video.currentTime = time;
-    //         event.target.scrollIntoView(); // Scroll the clicked paragraph into view
-    //     }
-    // });
+    transcript.addEventListener('click', (event) => {
+        if (event.target.tagName === 'p') {
+            const time = parseFloat(event.target.getAttribute('data-time'));
+            video.currentTime = time;
+            event.target.scrollIntoView(); // Scroll the clicked paragraph into view
+        }
+    });
 
 }); 
